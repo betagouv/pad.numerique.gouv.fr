5424e48b14ec8798803c63b0b1a16c66490a07ca
lebaudantoine <lebaud.antoine131@gmail.com>
2024-07-24 00:24:36 +0200

    🎨(frontend) hide tutorial if spell-checker is disabled
    Prevents displaying tutorial steps for elements not present in the DOM
    when the spell-checker is disabled.
    
    Also improved elements' naming for better readability.

--- public/js/lib/spell-checker/tutorial.js
+++ public/js/lib/spell-checker/tutorial.js
@@ -2,6 +2,13 @@ import introJs from 'intro.js'
 import 'intro.js/introjs.css'
 
 export function initTutorial () {
+  const spellCheckToggle = document.querySelector('.status-spellcheck')
+  const spellCheckStatus = document.querySelector('.spell-check-status')
+
+  if (!spellCheckToggle || !spellCheckStatus) {
+    return
+  }
+
   introJs().setOptions({
     nextLabel: 'Suivant',
     prevLabel: 'Précédent',
@@ -12,12 +19,12 @@ export function initTutorial () {
       {
         title: 'Nouveauté',
         intro: "La correction orthographique est disponible en version bêta. Vous pouvez l'activer ou la désactiver en cochant cette case.",
-        element: document.querySelector('.status-spellcheck')
+        element: spellCheckToggle
       },
       {
         title: 'Alertes et retours',
         intro: "Cette icône vous alerte en temps réel sur l'orthographe de votre document. \n\nLors de la rédaction d'un document, cliquez dessus pour nous partager vos retours sur la fonctionnalité.",
-        element: document.querySelector('.spell-check-status')
+        element: spellCheckStatus
       }
     ]
   }).start()
